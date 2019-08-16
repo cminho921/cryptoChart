4b6952a4dff7c7f122f2a62f6dd01753a01aa3cd
Minho Choo <mchoo7@gatech.edu>
2019-08-15 23:26:24 -0700

    fetch the api data to the client side

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,14 +4,31 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      coinData : []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://api.coindesk.com/v1/bpi/historical/close.json')
+      .then(res => {
+        if (res.status !== 200) {
+          console.log('Error, Status Code: ' + res.status);
+          return;
+        }
+        res.json().then(data => {
+          console.log(data);
+          this.setState({coinData: data})
+        });
+      })
+      .catch(err => {
+        log('Request failed', error)
+      });
   }
 
   render() {
     return (
       <div>
-        <div>test!</div>
+        <div>{this.state.coinData.disclaimer}</div>
       </div>
     )
   }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
+const router = express.Router();
+app.use(morgan('dev'));
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req,res) => res.send('Hello World'));
 app.use('/cryptoChart', express.static(path.resolve(__dirname, '..', 'client', 'dist')));
+
 
 
 app.listen(PORT, () => console.log(`App is listenning on port ${PORT}`))
